01f171043ef48ad0459502c74e49181e3e9d2698
ianmacdougald <ian@tartanltd.com>
2021-08-10 08:24:58 -0400

    Remove nsm session; add parameters

--- start_jack.sh
+++ start_jack.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
 
-SESSION=$1
+#set up parameters
+device=${device:-PCH}
+rate=${rate:-48000}
+nperiods=${nperiods:-2}
+period=${period:-64}
+
+#assign customized parameters
+while [ $# -gt 0 ]; do 
+    if [[ $1 == *"--"* ]]; then 
+	param="${1/--/}"
+	declare $param="$2"
+    fi
+
+    shift 
+done
 
 #reset pulse audio to default io
 pacmd "set-default-sink alsa_output.pci-0000_00_1f.3.analog-stereo"
@@ -8,38 +22,38 @@ pacmd "set-default-source alsa_input.pci-0000_00_1f.3.analog-stereo"
 
 echo "Resetting pulse audio"
 
-sleep 1.5
+sleep 1
 
 #kill jackdbus if it is running
 jackpid=$(pgrep jackdbus)
 if [ "$jackpid" ]; then 
-    echo "Killing: jackdbus"
+    echo "Killing jackdbus"
     kill -9 $jackpid
 fi 
 
 #kill a2jmidi daemon if it is running
 a2jpid=$(pgrep a2jmidid)
 if [ "$a2jpid" ]; then 
-    echo "Killing: a2jmidid"
+    echo "Killing a2jmidid"
     kill -9 $a2jpid
 fi
 
+#start jack
 jack_control start
 jack_control ds alsa
-jack_control dps device hw:M12
-jack_control dps rate 48000
-jack_control dps nperiods 2
-jack_control dps period 64
+jack_control dps device hw:$device
+jack_control dps rate $rate 
+jack_control dps nperiods $nperiods
+jack_control dps period $period 
+
 sleep 1 
+
+#start a2j midi
 a2j_control --ehw
 a2j_control --start
+
 sleep 1 
+
+#set the pulse audio sink
 pacmd "set-default-sink jack_out"
 pacmd "set-default-source jack_in"
-
-if [[ $# -eq 0 ]] ; then 
-    SESSION="basic_setup"
-fi
-
-echo "--- nsmd session -> $SESSION"
-nsmd --load-session $SESSION
