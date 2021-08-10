062175b0e73005a5cdb77d481caa5f2a0d0e5d09
ianmacdougald <ian@tartanltd.com>
2021-08-10 11:12:29 -0400

    Add installation instructions for new script

--- Makefile
+++ Makefile
@@ -1,4 +1,6 @@
 install: 
 	cp start_jack.sh /usr/local/bin/start_jack
+	cp stop_jack.sh /usr/local/bin/stop_jack
 uninstall: 
 	rm /usr/local/bin/start_jack
+	rm /usr/local/bin/stop_jack
--- start_jack.sh
+++ start_jack.sh
@@ -2,38 +2,53 @@
 
 VERSION=0.1.0
 
+PROG=$(basename $0)
+
 function usage {
-    echo "Usage: $(basename $0) [-drnpvh]" 2>&1
-    echo "start_jack - A quick bash script for starting, stopping, and configuring JACK"
-    echo "  -d	    Specify the audio device for the JACK server to use"
-    echo "  -r	    Set the sample rate for the server"
-    echo "  -n	    Set the number of periods for JACK (2 for PCI. 3 for USB)"
-    echo "  -p	    Set the number of samples per period"
-    echo "  -h	    Show help about command line options"
-    echo "  -u	    Uninstall start_jack"
-    echo "  -v	    Show version information"
+    echo "Usage: $PROG [-drnpvh]" 2>&1
+    echo "$PROG - A quick bash script for starting, stopping, and configuring JACK"
+    echo "  -d <param>	    Specify the audio device for the JACK server to use"
+    echo 
+    echo "  -h		    Show help about command line options"
+    echo
+    echo "  -k		    Kill jackd, a2jmidid, and QjackCtl"
+    echo
+    echo "  -n <param>	    Set the number of periods for JACK (2 for PCI. 3 for USB)"
+    echo
+    echo "  -p <param>	    Set the number of samples per period"
+    echo
+    echo "  -r <param>	    Set the sample rate for the server"
+    echo 
+    echo "  -u		    Uninstall $PROG"
+    echo 
+    echo "  -v		    Show version information"
     exit 1
 }
 
 function uninstall { 
-    echo "Uninstalling $(basename $0)"
-    sudo rm /usr/local/bin/start_jack
+    echo "Uninstalling $PROG"
+    sudo rm /usr/local/bin/$PROG
     exit 1
 }
 
 function version { 
-    echo "$(basename $0): $VERSION"
+    echo "$PROG: $VERSION"
     exit 1
 }
 
-DEVICE=PCH
+function kill_jack { 
+    stop_jack 
+    exit 1
+}
+
+stop_jack
+
+DEVICE="PCH,0"
 RATE=48000
 NPERIODS=2
 PERIOD=128
 
-options=":uvhdrnp"
-
-while getopts ${options} arg; do 
+while getopts ":d:r:n:p:vhuk" arg; do 
     case "${arg}" in 
 	d) 
 	    DEVICE="${OPTARG}"
@@ -56,6 +71,9 @@ while getopts ${options} arg; do
 	u)
 	    uninstall
 	    ;;
+	k)
+	    kill_jack 
+	    ;;
 	?) 
 	    echo "Invalid option: -${OPTARG}."
 	    echo 
@@ -68,37 +86,23 @@ done
 pacmd "set-default-sink alsa_output.pci-0000_00_1f.3.analog-stereo"
 pacmd "set-default-source alsa_input.pci-0000_00_1f.3.analog-stereo"
 
-echo "Resetting pulse audio"
+echo "--- resetting pulse audio"
 
 sleep 1
 
-#kill jackdbus if it is running
-jackpid=$(pgrep jackdbus)
-if [ "$jackpid" ]; then 
-    echo "Killing jackdbus"
-    kill -9 $jackpid
-fi 
-
-#kill a2jmidi daemon if it is running
-a2jpid=$(pgrep a2jmidid)
-if [ "$a2jpid" ]; then 
-    echo "Killing a2jmidid"
-    kill -9 $a2jpid
-fi
-
 #start jack
-jack_control start
 jack_control ds alsa
 jack_control dps device hw:$DEVICE
 jack_control dps rate $RATE 
 jack_control dps nperiods $NPERIODS
 jack_control dps period $PERIOD 
-
-sleep 1 
+jack_control start > /dev/null
+echo "--- starting jackd"
 
 #start a2j midi
 a2j_control --ehw
-a2j_control --start
+a2j_control --start > /dev/null
+echo "--- starting a2jmidid"
 
 sleep 1 
 
@@ -107,5 +111,5 @@ pacmd "set-default-sink jack_out"
 pacmd "set-default-source jack_in"
 
 #open qjackctl 
-echo "Opening qjackctl"
-qjackctl -s &.
+echo "--- starting qjackctl"
+qjackctl -s &
